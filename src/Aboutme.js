ffca8f1aa6bc1ef6642fa3ce0b7202bfbfdc5bb2
Madany Diallo <Madanyx@gmail.com>
2022-02-20 02:01:05 -0500

    updates
    updates

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -8,29 +8,24 @@ const StyledAboutme = styled.div`
   align-items: center;
   border-bottom: 0.9px solid black;
   display: flex;
+  justify-content: center;
+
+  h1 {
+    font-size: 40px;
+    display: flex;
     justify-content: center;
-
-
-
-h1{
-font-size: 40px;
-display: flex;
-justify-content: center;
-color: #DC143C;
-}
-
-
+    color: #dc143c;
+  }
 `;
 
 const Aboutme = () => {
   return (
     <StyledAboutme>
       <div>
-          <h1>About Me</h1>
-          <p>Hello my name is moussa diallo</p>
+        <h1>About Me</h1>
+        <p>Hello my name is Moussa Diallo I currently go to NYU and I'm a senior whose majoring in business</p>
       </div>
     </StyledAboutme>
   );
 };
 export default Aboutme;
-
